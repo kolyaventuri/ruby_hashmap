2661ad879535b48d25aa7aa44386287fd4d08203
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-09 21:21:50 -0600

    HashMap key calculation

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,4 +1,14 @@
 class HashMap
+  MAX_LENGTH = 10
+
   def [](key)
+  end
+
+  def _calculate(key)
+    chars = key.chars
+    codes = chars.map { |char| char.ord }
+    total = codes.sum
+
+    total % MAX_LENGTH
   end
 end
