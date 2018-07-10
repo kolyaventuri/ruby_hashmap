dcc7506d8147abeaa031b85e616ea7d0e4407cb4
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-09 21:05:08 -0600

    Linked list can add values

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,7 +1,13 @@
 class LinkedList
   attr_reader :data
+  attr_reader :next
 
   def initialize(value)
     @data = value
+    @next = nil
+  end
+
+  def add(value)
+    @next = LinkedList.new(value)
   end
 end
