d8a615d1b4100d2162788c7ea9dd6d3676e55b0a
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-09 21:19:51 -0600

    HHash value calculation spec

--- spec/hashmap_spec.rb
+++ spec/hashmap_spec.rb
@@ -6,4 +6,24 @@ describe HashMap do
 
     expect(hm['key']).to be_nil
   end
+
+  it 'should be able to calculate a numeric key' do
+    hm = HashMap.new
+
+    input = "Hello"
+    expected = 0 # Sum of ascii % length
+    result = hm._calculate(input)
+
+    expect(result).to be(expected)
+  end
+
+  it 'should be able to calculate a numeric key' do
+    hm = HashMap.new
+
+    input = "Jello"
+    expected = 2 # Sum of ascii % length
+    result = hm._calculate(input)
+
+    expect(result).to be(expected)
+  end
 end
