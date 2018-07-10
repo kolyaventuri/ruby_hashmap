9301182dc5a01cc3bdbd131d08c67b5ba91a34a5
Kolya Venturi <kolya.venturi@enx3s.com>
2018-07-09 21:04:28 -0600

    Test that linked list can accept data

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,11 +1,24 @@
 require './lib/linked_list'
 
 describe LinkedList do
-  let(:data) { 10 }
+  let(:data)  { 10 }
+  let(:data2) { 15 }
+  let(:data3) { 20 }
 
   it 'should hold data' do
     ll = LinkedList.new(data)
 
     expect(ll.data).to be(data)
+    expect(ll.next).to be_nil
+  end
+
+  it 'should accept a new node' do
+    ll = LinkedList.new(data)
+
+    ll.add(data2)
+
+    expect(ll.next).to_not be_nil
+    expect(ll.next).to be_instance_of(LinkedList)
+    expect(ll.next.data).to be(data2)
   end
 end
